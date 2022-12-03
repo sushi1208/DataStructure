406226f7ef3c1237898d5d167db4720ec9e54f12
Ayush <ayushbahl12@gmail.com>
2022-12-03 17:05:33 +0530

    update

--- Array_To_BST.c
+++ Array_To_BST.c
@@ -1,74 +1,124 @@
+// Solution to Question 3-a of the Midterm Exam (Take-Home)
+// Read https://www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/ once; however wrote code on my own
+
 #include <stdio.h>
 #include <stdlib.h>
- 
-struct node
+
+struct nodeType
 {
-    int key;
-    struct node *left, *right;      // nodes initialized for left and right subtrees
+	int val;
+	struct nodeType* left; 
+	struct nodeType* right;
 };
- 
-struct node* newNode(int a)      // function to create a binary search tree
+
+typedef struct nodeType* NodeAddress;
+
+NodeAddress createnode(int a)
 {
-    struct node* bst = (struct node*)malloc(sizeof(struct node));      // allocating memory for node
-    bst->key = a;
-    bst->left = bst->right = NULL;
-    return bst;                         // returning bst node to the main function
-}
+    struct nodeType* node = malloc(sizeof(struct nodeType));                // allocating memory using malloc
+    node->left = NULL;
+    node->right = NULL;
+    node->val = a;
  
-void traverse(struct node* root)        // function to traverse and print the BST
+    return node;
+}
+
+NodeAddress insert(NodeAddress root, int val)
+{
+	if(root == NULL)
+    {
+        return createnode(val);
+    }
+
+    if (val > root->val)                                   
+    {
+        root->right = insert(root->right, val);
+    }
+	else if (val < root->val)                              
+    {
+        root->left = insert(root->left, val);
+    }
+
+	return root;
+}
+
+
+NodeAddress arrayToBST(int *a, int n)
+{
+    int midpoint = (n)/2;                                  // finding the midpoint of the sorted array
+    
+    struct nodeType *root = createnode(a[midpoint]);       // creating the root of the BST 
+ 
+    for(int i = 0; i < n; i++)
+    {
+        if (root == NULL)
+        {
+            return createnode(a[i]);
+        }
+        
+        else if(a[i] < root->val) 
+        {
+            root->left  = insert(root->left , a[i]);         // which tree the element goes into for the BST
+        }
+	
+        else if (a[i] > root->val )
+        {
+            root->right = insert(root->right, a[i]);         // which tree the element goes into for the BST
+        }
+    }
+
+    return root;
+}
+
+void prefixPrint(NodeAddress root)        // function to print the BST using prefix order
 {
     if (root != NULL)
     {
-        traverse(root->left);
-        printf("%d \n", root->key);
-        traverse(root->right);
+        prefixPrint(root->left);
+        printf("%d ", root->val);
+        prefixPrint(root->right);
     }
 }
- 
-struct node* insert(struct node* a, int b)
-{
-    if (a == NULL)           // condition when tree is empty
-        return newNode(b);
- 
-    if (b < a->key)
-    {
-        a->left = insert(a->left, b);
-    }
-    else if (b > a->key)
-    {
-        a->right = insert(a->right, b);
-    }
- 
-    return a;           // returning original pointer
-}
- 
+
+
+
 int main()
 {
-    
-    int length = 0;
+  
+    int length;
 
     printf("Please enter the number of elements in your array:\n");
 
     scanf("%d", &length);
     
     printf("Please enter the elements of your array:\n");
+    
+    int *a1;                                                // creating a dynamic array
 
-    int array_input[length];
-
+    a1 = (int*)malloc(length * sizeof(int));
     
     for(int i = 0; i < length; ++i)
     {
-        scanf("%d", &array_input[i]);
+        scanf("%d", &a1[i]);
+    }
+
+    for (int  i = 0; i < length - 1; i++)               // sorting the array inputted using selection sort
+    {
+        int min = i;
+
+        for (int j = i + 1; j < length; j++)
+        {
+            if (a1[j] < a1[min])
+            {
+                int temp = a1[min];    
+                a1[min] = a1[j];          
+                a1[j] = temp;
+            }
+        }
     }
     
-    struct node* root = NULL;
-    
-    root = insert(root, array_input[0]);                // inserting the root first
-    for(int i = 1; i < length; ++i)                     
-    {
-        insert(root, array_input[i]);                   // inserting the elements of the array into the BST
-    } 
+    printf("BST:  \n");
 
-    traverse(root);                                     // printing the BST
- 
+    prefixPrint(arrayToBST(a1, length));
+
 }
